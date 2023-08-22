c02d5fb5e91be4230afb7f2e48bd6ee0f115c394
Krishna Kumar <krishna.kumar@arthmate.co.in>
2023-08-22 12:18:45 +0530

    add cors policy in backend

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-var cors = require("cors");
+import cors from "cors";
 
 import Routes from "./Routes/index.js";
 
