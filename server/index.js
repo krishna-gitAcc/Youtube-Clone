25afcb624efcc81c37d345081a1799d94bbb7b7e
Krishna Kumar <krishna.kumar@arthmate.co.in>
2023-08-24 15:36:57 +0530

    updated the server.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,15 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-app.options("*", cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, company_code"
+  );
+  next();
+});
 
 dotenv.config();
 
